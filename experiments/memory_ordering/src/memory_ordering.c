803fb1eceff2a4fc37fe2ad084dde410b7e7d53c
ArchWayland <vansi.ganeshs@gmail.com>
2025-07-11 11:54:55 -0400

    perf: optimize the cpu thread scheduling to increase reordering probability
    - Add Thread to CPU pinning for determinsitic stress testing
    - An atomic sync fetch and add on a global iteration counter for logging
    - Increase iterations to 100M per thread for better results
    - Sucessfully detected reordering at an average 100Million iterations

--- experiments/memory_ordering/src/memory_ordering.c
+++ experiments/memory_ordering/src/memory_ordering.c
@@ -1,25 +1,37 @@
+#define _GNU_SOURCE
 #include <pthread.h>
 #include <sched.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#define ITERS 1000000
+#define ITERS 100000000
 
 volatile int thread_flag_0 = 0;
 volatile int thread_flag_1 = 0;
+volatile long iteration_counter = 0;
 
 typedef struct {
 
     int id;
-    int reorder_flag;
+    volatile int reorder_flag;
+    volatile int running_flag;
 } reorder_t;
 
 void *set_flag(void *id_ret) {
 
     reorder_t *reorder = (reorder_t *)id_ret;
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(reorder->id, &cpuset);
+    pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
 
-    for (int i = 0; i < ITERS; i++) {
+    for (long i = 0; i < ITERS; i++) {
+
+        __sync_fetch_and_add(&iteration_counter, 1);
+        if (i % 1000 == 0) sched_yield();
+
+
         if (reorder->id == 0) {
 
             thread_flag_0 = 1;
@@ -33,14 +45,13 @@ void *set_flag(void *id_ret) {
                  // double check if other thread raised flag (it raised its flag after thread 0 check)
             }
             thread_flag_0 = 0;
-        } else if (reorder->id == 1) {
+        } else if (reorder->id == 7) {
 
             thread_flag_1 = 1;
             if (thread_flag_0 == 0) {
 
                 if (thread_flag_0 == 1) {
 
-                    printf("Instruction has been reordered!\n");
                     reorder->reorder_flag = 1;
                     break;
                 }
@@ -48,6 +59,8 @@ void *set_flag(void *id_ret) {
             thread_flag_1 = 0;
         }
     }
+
+    reorder->running_flag = 0;
     return NULL;
 
 }
@@ -59,8 +72,10 @@ int main(int argc, char *argv[]) {
     reorder_t reorder_t0, reorder_t1;
     reorder_t0.id = 0;
     reorder_t0.reorder_flag = 0;
-    reorder_t1.id = 1;
+    reorder_t0.running_flag = 1;
+    reorder_t1.id = 7;
     reorder_t1.reorder_flag = 0;
+    reorder_t1.running_flag = 1;
 
     if (pthread_create(&thread_0, NULL, set_flag, &reorder_t0) != 0) {
 
@@ -74,23 +89,32 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    while (reorder_t0.running_flag && reorder_t1.running_flag) {
+        
+        printf("Program is running...\n");
+        printf("Number of iterations run until now: %ld\n", iteration_counter);
+        sleep(1);
+    }
+
     if (pthread_join(thread_0, NULL) != 0) {
 
-        perror("Failed to create thread 0");
+        perror("Failed to join thread 0");
         exit(1);
-    } 
+    }
 
     if (pthread_join(thread_1, NULL) != 0) {
 
-        perror("Failed to create thread 1");
+        perror("Failed to join thread 1");
         exit(1);
-    } 
+    }
 
     if (reorder_t0.reorder_flag || reorder_t1.reorder_flag) {
 
-        printf("Instruction has been reordered!\n");
+        printf("Instruction has been reordered! Completed %ld iterations\n", 
+                iteration_counter); 
     } else {
-        printf("Completed 1000000 iterations, no reordering occured\n");
+        printf("Completed %ld iterations, no reordering occured\n",
+                iteration_counter); 
     }
     return 0;
 }
